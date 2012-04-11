c53c41df2e2ea36d0e1f4c3dffd77c87ba6306a5
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-04-10 21:32:13 -0700

    add bootstrap

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"path/filepath"
 )
 
 const (
@@ -10,7 +14,7 @@ const (
 	// information about users and projects. All in
 	// one place because I expect this information to be
 	// very small
-	DataFileName = "apptranslator.dat"
+	DataFileName = "apptranslator.js"
 )
 
 type User struct {
@@ -20,26 +24,86 @@ type User struct {
 type Project struct {
 	Name      string
 	AdminUser string // corresponds to User.Login field
+	// a secret value that must be provided when uploading
+	// new strings for translation
+	UploadSecret string
 }
 
-func readDataAtStartup(fileName string) error {
-	if file, err := os.Open(fileName); err != nil {
-		if file, err = os.Create(fileName); err != nil {
-			return err
-		}
-		file.Close()
-		return nil
-	} else {
-		// TODO: read the data
-		file.Close()
+type AppState struct {
+	Users    []User
+	Projects []Project
+}
+
+var (
+	appState  = AppState{}
+	staticDir = filepath.Join("www", "static")
+)
+
+func saveData() {
+	b, err := json.Marshal(&appState)
+	if err != nil {
+		fmt.Printf("FileStorage.SetJSON, Marshal json failed (%v):%s\n", appState, err)
+		return
 	}
-	return nil
+	ioutil.WriteFile(DataFileName, b, 0600)
+}
+
+func readDataAtStartup() error {
+	b, err := ioutil.ReadFile(DataFileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	return json.Unmarshal(b, &appState)
+}
+
+func serve404(w http.ResponseWriter) {
+	fmt.Fprint(w, `<html><body>Page Not Found!</body></html>`)
+}
+
+func serveFileFromDir(w http.ResponseWriter, r *http.Request, dir, fileName string) {
+	filePath := filepath.Join(dir, fileName)
+	http.ServeFile(w, r, filePath)
+	/*
+		b, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("serveFileFromDir() file=%s doesn't exist\n", filePath)
+			serve404(w)
+			return
+		}
+		w.Write(b)	
+		fmt.Printf("serveFileFromDir() served %d bytes of '%s'\n", len(b), filePath)
+	*/
+}
+
+func serveFileStatic(w http.ResponseWriter, r *http.Request, fileName string) {
+	serveFileFromDir(w, r, staticDir, fileName)
+}
+
+const lenStatic = len("/static/")
+
+func handleStatic(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Path[lenStatic:]
+	serveFileStatic(w, r, file)
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	serveFileFromDir(w, r, "www", "main.html")
 }
 
 func main() {
-	if err := readDataAtStartup(DataFileName); err != nil {
+	if err := readDataAtStartup(); err != nil {
 		fmt.Printf("Failed to open data file %s. Can't proceed.\n", DataFileName)
 		return
 	}
 	fmt.Printf("Read the data from %s\n", DataFileName)
+
+	http.HandleFunc("/static/", handleStatic)
+	http.HandleFunc("/", handleMain)
+
+	port := ":8888"
+	fmt.Printf("Running on %s\n", port)
+	http.ListenAndServe(port, nil)
 }
