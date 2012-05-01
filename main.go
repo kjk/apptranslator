dd8ad916562736ec5bb505d3af560274e7a87b6d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-04-30 18:53:45 -0700

    add /downloadtranslations

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
@@ -335,7 +336,7 @@ func serveFileFromDir(w http.ResponseWriter, r *http.Request, dir, fileName stri
 			serve404(w)
 			return
 		}
-		w.Write(b)	
+		w.Write(b)
 		fmt.Printf("serveFileFromDir() served %d bytes of '%s'\n", len(b), filePath)
 	*/
 }
@@ -575,6 +576,61 @@ func handleApp(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type LangTrans struct {
+	lang  string
+	trans string
+}
+
+// handler for url: /downloadtranslations?name=$app
+// Returns plain/text response in the format:
+/*
+AppTranslator: $appName
+:string 1
+cv:translation for cv language
+pl:translation for pl language
+:string 2
+pl:translation for pl language
+*/
+// This format is designed for easy parsing
+// TODO: allow for avoiding re-downloading of translations if they didn't
+// change via some ETag-like or content hash functionality
+func handleDownloadTranslations(w http.ResponseWriter, r *http.Request) {
+	appName := strings.TrimSpace(r.FormValue("name"))
+	app := findApp(appName)
+	if app == nil {
+		serverErrorMsg(w, fmt.Sprintf("Application '%s' doesn't exist", appName))
+		return
+	}
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	io.WriteString(w, fmt.Sprintf("AppTranslator: %s\n", app.Name))
+	m := make(map[string][]LangTrans)
+	for _, li := range app.langInfos {
+		code := li.Code
+		for _, t := range li.Translations {
+			if "" == t.Current() {
+				continue
+			}
+			s := t.String
+			l, exists := m[s]
+			if !exists {
+				l = make([]LangTrans, 0)
+			}
+			var lt LangTrans
+			lt.lang = code
+			lt.trans = t.Current()
+			l = append(l, lt)
+			m[s] = l
+		}
+	}
+
+	for s, ltarr := range m {
+		io.WriteString(w, fmt.Sprintf(":%s\n", s))
+		for _, lt := range ltarr {
+			io.WriteString(w, fmt.Sprintf("%s:%s\n", lt.lang, lt.trans))
+		}
+	}
+}
+
 // handler for url: /edittranslation
 func handleEditTranslation(w http.ResponseWriter, r *http.Request) {
 	appName := strings.TrimSpace(r.FormValue("app"))
@@ -618,6 +674,7 @@ func main() {
 	http.HandleFunc("/static/", handleStatic)
 	http.HandleFunc("/addapp", handleAddApp)
 	http.HandleFunc("/app/", handleApp)
+	http.HandleFunc("/downloadtranslations", handleDownloadTranslations)
 	http.HandleFunc("/edittranslation", handleEditTranslation)
 	http.HandleFunc("/", handleMain)
 
