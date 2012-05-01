ff64bc8d354faa0ea62a40636fc9b0577b232036
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-04-30 19:33:12 -0700

    start on /handleUploadStrings

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"html/template"
@@ -152,7 +153,7 @@ func (a *App) translationLogFilePath() string {
 }
 
 func (a *App) writeTranslationToLog(langCode, str, trans string) {
-	fmt.Printf("writeTranslationToLog %s:%s => %s\n", langCode, str, trans)
+	//fmt.Printf("writeTranslationToLog %s:%s => %s\n", langCode, str, trans)
 	a.mu.Lock()
 	defer a.mu.Unlock()
 	p := a.translationLogFilePath()
@@ -204,10 +205,15 @@ func (a *App) UntranslatedCount() int {
 }
 
 func (app *App) addTranslation(langCode, str, trans string, allowNew bool) {
-	_, exists := app.allStrings[str]
-	if !exists && !allowNew {
-		fmt.Printf("Skipped %s\n", str)
-		return
+	obsolete, exists := app.allStrings[str]
+	if !exists {
+		if !allowNew {
+			fmt.Printf("addTranslation(): skipped %s because not allowing adding strings\n", str)
+			return
+		}
+		// if adding new, it's not obsolete, otherwise we preserve the
+		// previous value
+		obsolete = false
 	}
 	for _, li := range app.langInfos {
 		if li.Code == langCode {
@@ -217,7 +223,7 @@ func (app *App) addTranslation(langCode, str, trans string, allowNew bool) {
 		}
 	}
 	if !exists {
-		app.allStrings[str] = true
+		app.allStrings[str] = obsolete
 	}
 }
 
@@ -658,6 +664,94 @@ func handleEditTranslation(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type CantParseError struct {
+	Msg    string
+	LineNo int
+}
+
+func (e *CantParseError) Error() string {
+	return fmt.Sprintf("Error: %s on line %d", e.Msg, e.LineNo)
+}
+
+func myReadLine(r *bufio.Reader) (string, error) {
+	line, isPrefix, err := r.ReadLine()
+	if err != nil {
+		return "", err
+	}
+	if isPrefix {
+		return "", &CantParseError{"Line too long", -1}
+	}
+	return string(line), nil
+}
+
+// returns nil on error
+func parseUploadedStrings(reader io.Reader) []string {
+	r := bufio.NewReaderSize(reader, 4*1024)
+	res := make([]string, 0)
+	parsedHeader := false
+	for {
+		line, err := myReadLine(r)
+		if err != nil {
+			if err == io.EOF {
+				if 0 == len(res) {
+					return nil
+				}
+				return res
+			}
+			return nil
+		}
+		if !parsedHeader {
+			// first line must be: AppTranslator strings
+			if line != "AppTranslator strings" {
+				fmt.Printf("parseUploadedStrings(): invalid first line: '%s'\n", line)
+				return nil
+			}
+			parsedHeader = true
+		} else {
+			if 0 == len(line) {
+				fmt.Printf("parseUploadedStrings(): encountered empty line\n")
+				return nil
+			}
+			res = append(res, line)
+		}
+	}
+	return res
+}
+
+// handler for url: POST /handleUploadStrings?app=$appName&secret=$uploadSecret
+// POST data is in the format:
+/*
+AppTranslator strings
+string to translate 1
+string to translate 2
+...
+*/
+func handleUploadStrings(w http.ResponseWriter, r *http.Request) {
+	appName := strings.TrimSpace(r.FormValue("app"))
+	app := findApp(appName)
+	if app == nil {
+		serverErrorMsg(w, fmt.Sprintf("Application '%s' doesn't exist", appName))
+		return
+	}
+	secret := strings.TrimSpace(r.FormValue("secret"))
+	if secret != app.UploadSecret {
+		serverErrorMsg(w, fmt.Sprintf("Invalid secret for app '%s'", appName))
+		return
+	}
+	file, _, err := r.FormFile("strings")
+	if err != nil {
+		serverErrorMsg(w, fmt.Sprintf("No file with strings %s", err.Error()))
+		return
+	}
+	defer file.Close()
+	newStrings := parseUploadedStrings(file)
+	if nil == newStrings {
+		serverErrorMsg(w, "Error parsing uploaded strings")
+		return
+	}
+	w.Write([]byte("Ok\n"))
+}
+
 func main() {
 	if err := readDataAtStartup(); err != nil {
 		fmt.Printf("Failed to open data file %s. Can't proceed.\n", dataFileName)
@@ -676,6 +770,7 @@ func main() {
 	http.HandleFunc("/app/", handleApp)
 	http.HandleFunc("/downloadtranslations", handleDownloadTranslations)
 	http.HandleFunc("/edittranslation", handleEditTranslation)
+	http.HandleFunc("/uploadstrings", handleUploadStrings)
 	http.HandleFunc("/", handleMain)
 
 	port := ":8890"
