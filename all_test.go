a11e06c204924a41037916bd4688ad4500158cf4
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-09-07 12:04:42 -0700

    restructure tests

--- all_test.go
+++ all_test.go
@@ -8,96 +8,115 @@ import (
 	"testing"
 )
 
-func errIf(t *testing.T, cond bool, msg string) {
-	if cond {
-		t.Error(msg)
-	}
+type TestState struct {
+	t *testing.T
+	// ether l is set or both s and w
+	l *TranslationLog
+
+	s *EncoderDecoderState
+	w io.Writer
 }
 
-func ensureLangCount(t *testing.T, s *EncoderDecoderState, expected int) {
+func (ts *TestState) getEncoderDecoderState() *EncoderDecoderState {
+	if nil != ts.s {
+		return ts.s
+	}
+	return ts.l.state
+}
+
+func (ts *TestState) ensureLangCount(expected int) {
+	s := ts.getEncoderDecoderState()
 	if len(s.langCodeMap) != expected {
-		t.Fatalf("len(s.langCodeMap)=%d, expected: %d", len(s.langCodeMap), expected)
+		ts.t.Fatalf("len(s.langCodeMap)=%d, expected: %d", len(s.langCodeMap), expected)
 	}
 }
 
-func ensureUserCount(t *testing.T, s *EncoderDecoderState, expected int) {
+func (ts *TestState) ensureUserCount(expected int) {
+	s := ts.getEncoderDecoderState()
 	if len(s.userNameMap) != expected {
-		t.Fatalf("len(s.userNameMap)=%d, expected: %d", len(s.userNameMap), expected)
+		ts.t.Fatalf("len(s.userNameMap)=%d, expected: %d", len(s.userNameMap), expected)
 	}
 }
 
-func ensureStringsCount(t *testing.T, s *EncoderDecoderState, expected int) {
+func (ts *TestState) ensureStringsCount(expected int) {
+	s := ts.getEncoderDecoderState()
 	if len(s.stringMap) != expected {
-		t.Fatalf("len(s.stringMap)=%d, expected: %d", len(s.stringMap), expected)
+		ts.t.Fatalf("len(s.stringMap)=%d, expected: %d", len(s.stringMap), expected)
 	}
 }
 
-func ensureUndeletedStringsCount(t *testing.T, s *EncoderDecoderState, expected int) {
+func (ts *TestState) ensureUndeletedStringsCount(expected int) {
+	s := ts.getEncoderDecoderState()
 	n := s.stringsCount()
 	if n != expected {
-		t.Fatalf("len(s.stringsCount())=%d, expected: %d", s.stringsCount(), expected)
+		ts.t.Fatalf("len(s.stringsCount())=%d, expected: %d", s.stringsCount(), expected)
 	}
 }
 
-func ensureTranslationsCount(t *testing.T, s *EncoderDecoderState, expected int) {
+func (ts *TestState) ensureTranslationsCount(expected int) {
+	s := ts.getEncoderDecoderState()
 	if len(s.translations) != expected {
-		t.Fatalf("len(s.translations)=%d, expected: %d", len(s.translations), expected)
+		ts.t.Fatalf("len(s.translations)=%d, expected: %d", len(s.translations), expected)
 	}
 }
 
-func ensureLangCode(t *testing.T, s *EncoderDecoderState, name string, expected int) {
+func (ts *TestState) ensureLangCode(name string, expected int) {
+	s := ts.getEncoderDecoderState()
 	if s.langCodeMap[name] != expected {
-		t.Fatalf("s.langCodeMap['%s']=%d, expected: %d", s.langCodeMap[name], expected)
+		ts.t.Fatalf("s.langCodeMap['%s']=%d, expected: %d", s.langCodeMap[name], expected)
 	}
 }
 
-func ensureUserCode(t *testing.T, s *EncoderDecoderState, name string, expected int) {
+func (ts *TestState) ensureUserCode(name string, expected int) {
+	s := ts.getEncoderDecoderState()
 	if s.userNameMap[name] != expected {
-		t.Fatalf("s.userNameMap['%s']=%d, expected: %d", s.userNameMap[name], expected)
+		ts.t.Fatalf("s.userNameMap['%s']=%d, expected: %d", s.userNameMap[name], expected)
 	}
 }
 
-func ensureStringCode(t *testing.T, s *EncoderDecoderState, name string, expected int) {
+func (ts *TestState) ensureStringCode(name string, expected int) {
+	s := ts.getEncoderDecoderState()
 	if s.stringMap[name] != expected {
-		t.Fatalf("s.stringMap['%s']=%d, expected: %d", s.stringMap[name], expected)
+		ts.t.Fatalf("s.stringMap['%s']=%d, expected: %d", s.stringMap[name], expected)
 	}
 }
 
-func ensureDeletedCount(t *testing.T, s *EncoderDecoderState, expected int) {
+func (ts *TestState) ensureDeletedCount(expected int) {
+	s := ts.getEncoderDecoderState()
 	if len(s.deletedStrings) != expected {
-		t.Fatalf("len(s.deletedStrings)=%d, expected: %d", len(s.deletedStrings), expected)
+		ts.t.Fatalf("len(s.deletedStrings)=%d, expected: %d", len(s.deletedStrings), expected)
 	}
 }
 
-func ensureStateEmpty(t *testing.T, s *EncoderDecoderState) {
-	ensureLangCount(t, s, 0)
-	ensureUserCount(t, s, 0)
-	ensureStringsCount(t, s, 0)
+func (ts *TestState) ensureStateEmpty() {
+	ts.ensureLangCount(0)
+	ts.ensureUserCount(0)
+	ts.ensureStringsCount(0)
 }
 
-func ensureStateAfter1(t *testing.T, s *EncoderDecoderState) {
-	ensureLangCount(t, s, 1)
-	ensureLangCode(t, s, "us", 1)
-	ensureUserCount(t, s, 1)
-	ensureUserCode(t, s, "user1", 1)
-	ensureStringsCount(t, s, 1)
-	ensureTranslationsCount(t, s, 1)
+func (ts *TestState) ensureStateAfter1() {
+	ts.ensureLangCount(1)
+	ts.ensureLangCode("us", 1)
+	ts.ensureUserCount(1)
+	ts.ensureUserCode("user1", 1)
+	ts.ensureStringsCount(1)
+	ts.ensureTranslationsCount(1)
 }
 
-func ensureStateAfter2(t *testing.T, s *EncoderDecoderState) {
-	ensureLangCount(t, s, 2)
-	ensureLangCode(t, s, "pl", 2)
-	ensureUserCount(t, s, 1)
-	ensureTranslationsCount(t, s, 2)
+func (ts *TestState) ensureStateAfter2() {
+	ts.ensureLangCount(2)
+	ts.ensureLangCode("pl", 2)
+	ts.ensureUserCount(1)
+	ts.ensureTranslationsCount(2)
 }
 
-func ensureStateAfter3(t *testing.T, s *EncoderDecoderState) {
-	ensureLangCount(t, s, 2)
-	ensureUserCount(t, s, 1)
-	ensureLangCode(t, s, "us", 1)
-	ensureUserCode(t, s, "user1", 1)
-	ensureLangCode(t, s, "pl", 2)
-	ensureTranslationsCount(t, s, 3)
+func (ts *TestState) ensureStateAfter3() {
+	ts.ensureLangCount(2)
+	ts.ensureUserCount(1)
+	ts.ensureLangCode("us", 1)
+	ts.ensureUserCode("user1", 1)
+	ts.ensureLangCode("pl", 2)
+	ts.ensureTranslationsCount(3)
 }
 
 func NewTranslationLogEnsure(t *testing.T, path string) *TranslationLog {
@@ -114,26 +133,30 @@ func TestTransLog2(t *testing.T) {
 	os.Remove(path) // just in case
 
 	l := NewTranslationLogEnsure(t, path)
-	ensureStateEmpty(t, l.state)
+	ts := &TestState{t, l, nil, nil}
+
+	ts.ensureStateEmpty()
+
 	err := l.writeNewTranslation("foo", "foo-us", "us", "user1")
 	if err != nil {
 		t.Fatal(err)
 	}
-	ensureStateAfter1(t, l.state)
+	ts.ensureStateAfter1()
 	l.close()
 
 	l = NewTranslationLogEnsure(t, path)
-	ensureStateAfter1(t, l.state)
+	ts = &TestState{t, l, nil, nil}
+	ts.ensureStateAfter1()
 
 	err = l.writeNewTranslation("foo", "foo-pl", "pl", "user1")
 	if err != nil {
 		t.Fatal(err)
 	}
-	ensureStateAfter2(t, l.state)
+	ts.ensureStateAfter2()
 	l.close()
 
 	l = NewTranslationLogEnsure(t, path)
-	ensureStateAfter2(t, l.state)
+	ts.ensureStateAfter2()
 	l.close()
 }
 
@@ -149,30 +172,34 @@ func TestTransLog(t *testing.T) {
 	var err error
 
 	s := NewEncoderDecoderState()
-	ensureStateEmpty(t, s)
+	ts := &TestState{t, nil, s, &buf}
+	ts.ensureStateEmpty()
 
 	writeNewTranslationEnsure(t, s, &buf, "foo", "foo-us", "us", "user1")
-	ensureStateAfter1(t, s)
+	ts.ensureStateAfter1()
 
 	writeNewTranslationEnsure(t, s, &buf, "foo", "foo-pl", "pl", "user1")
-	ensureStateAfter2(t, s)
+	ts.ensureStateAfter2()
 
 	writeNewTranslationEnsure(t, s, &buf, "bar", "bar-pl", "pl", "user1")
-	ensureStateAfter3(t, s)
+	ts.ensureStateAfter3()
 
 	err = s.deleteString(&buf, "bar")
 	if err != nil {
 		t.Fatal(err)
 	}
-	ensureDeletedCount(t, s, 1)
-	ensureUndeletedStringsCount(t, s, 1)
+	ts.ensureDeletedCount(1)
+	ts.ensureUndeletedStringsCount(1)
 
 	err = s.undeleteString(&buf, "bar")
 	if err != nil {
 		t.Fatal(err)
 	}
-	ensureDeletedCount(t, s, 0)
-	ensureUndeletedStringsCount(t, s, 2)
+	ts.ensureDeletedCount(0)
+	ts.ensureUndeletedStringsCount(2)
+
+	//newStrings := []string{"foo", "bar", "go"}
+	//ts.updateStringsListEnsure(newStrings)
 
 	fmt.Printf("buf.Bytes()=%v\n", buf.Bytes())
 
@@ -182,5 +209,5 @@ func TestTransLog(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	ensureStateAfter3(t, s)
+	ts.ensureStateAfter3()
 }
