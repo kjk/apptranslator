188b120f9ee0792cc2b48bb090847cf64b83898c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-09-06 17:23:10 -0700

    update build.sh for recent code restructuring

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 export GOPATH=$GOPATH:`pwd`/ext
-go build -o apptranslator main.go util.go translog.go translog2.go langs.go 
-go build -o importsumtrans translog2.go util.go langs.go importsumtrans.go
+go build -o apptranslator main.go util.go translog.go handler_*.go langs.go 
+go build -o importsumtrans translog.go util.go langs.go importsumtrans.go
 rm importsumtrans apptranslator
