d27925b246a14f739f8036ab1c7d0e717ee9391d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-09-20 12:25:51 -0700

    move importsum to its own directory

--- scripts/importsum.sh
+++ scripts/importsum.sh
@@ -1,1 +1,9 @@
-go run translog.go util.go langs.go importsumtrans.go
+#!/bin/bash
+cp translog.go util.go langs.go importsumatra
+cd importsumatra
+go build -o importsum *.go
+if [ "$?" -ne 0 ]; then echo "failed to build"; exit 1; fi 
+rm translog.go util.go langs.go # we used you so now we discard you
+cd ..
+./importsumatra/importsum
+rm importsumatra/importsum
