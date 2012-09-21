357538e059f523d087e659a2d3eaeb869f6bf171
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-09-21 14:37:57 -0700

    move zip construction to a separate function

--- s3backup.go
+++ s3backup.go
@@ -91,50 +91,49 @@ func ensureValidConfig(config *BackupConfig) {
 	fmt.Printf("s3 bucket ok!\n")
 }
 
-func doBackup(config *BackupConfig) {
-	// TODO: a better way to generate a random file name
-	path := filepath.Join(os.TempDir(), "apptranslator-tmp-backup.zip")
-	fmt.Printf("zip file name: %s\n", path)
-	os.Remove(path)
-	zf, err := os.Create(path)
+// the names of files inside the zip file are relatitve to dirToZip e.g.
+// if dirToZip is foo and there is a file foo/bar.txt, the name in the zip
+// will be bar.txt
+func createZipWithDirContent(zipFilePath, dirToZip string) error {
+	if isDir, err := PathIsDir(dirToZip); err != nil || !isDir {
+		// TODO: should return an error if err == nil && !isDir
+		return err
+	}
+	zf, err := os.Create(zipFilePath)
 	if err != nil {
-		// TODO: what to do about it? Notify using e-mail?
-		return
+		fmt.Printf("Failed to os.Create() %s, %s\n", zipFilePath, err.Error())
+		return err
 	}
 	defer zf.Close()
-	//defer os.Remove(path)
 	zipWriter := zip.NewWriter(zf)
 	// TODO: is the order of defer here can create problems?
 	// TODO: need to check error code returned by Close()
 	defer zipWriter.Close()
 
 	//fmt.Printf("Walk root: %s\n", config.LocalDir)
-	root := config.LocalDir
-	rootLen := len(config.LocalDir) + 1 // +1 for slash
-	err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	err = filepath.Walk(dirToZip, func(pathToZip string, info os.FileInfo, err error) error {
 		if err != nil {
+			fmt.Printf("WalkFunc() received err %s from filepath.Wath()\n", err.Error())
 			return err
 		}
 		//fmt.Printf("%s\n", path)
-		if root == path {
-			return nil
-		}
-		toZipPath := filepath.Join(root, path[rootLen:])
-		fmt.Printf("toZipPath: %s\n", toZipPath)
-		isDir, err := PathIsDir(toZipPath)
+		isDir, err := PathIsDir(pathToZip)
 		if err != nil {
+			fmt.Printf("PathIsDir() for %s failed with %s\n", pathToZip, err.Error())
 			return err
 		}
 		if isDir {
 			return nil
 		}
-		toZipReader, err := os.Open(toZipPath)
+		toZipReader, err := os.Open(pathToZip)
 		if err != nil {
+			fmt.Printf("os.Open() %s failed with %s\n", pathToZip, err.Error())
 			return err
 		}
 		defer toZipReader.Close()
 
-		inZipWriter, err := zipWriter.Create(toZipPath)
+		zipName := pathToZip[len(dirToZip)+1:] // +1 for '/' in the path
+		inZipWriter, err := zipWriter.Create(zipName)
 		if err != nil {
 			fmt.Printf("Error in zipWriter(): %s\n", err.Error())
 			return err
@@ -143,9 +142,21 @@ func doBackup(config *BackupConfig) {
 		if err != nil {
 			return err
 		}
-		fmt.Printf("Added %s to zip file\n", toZipPath)
+		fmt.Printf("Added %s to zip file\n", pathToZip)
 		return nil
 	})
+	return nil
+}
+
+// TODO: what to do about failures? Log somewhere and allow viewing via website?
+// e-mail the failures once a day?
+func doBackup(config *BackupConfig) {
+	// TODO: a better way to generate a random file name
+	path := filepath.Join(os.TempDir(), "apptranslator-tmp-backup.zip")
+	fmt.Printf("zip file name: %s\n", path)
+	os.Remove(path) // remove before trying to create a new one, just in cased
+	err := createZipWithDirContent(path, config.LocalDir)
+	defer os.Remove(path)
 	if err != nil {
 		return
 	}
