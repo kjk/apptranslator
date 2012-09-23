00a7e46e76f3bdb6b7113587635c906f4fa9dea1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2012-09-22 22:24:43 -0700

    move importsumatra to tools/importsumatra

--- tools/importsumatra/run.sh
+++ tools/importsumatra/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cp translog.go util.go langs.go importsumatra
-cd importsumatra
+cp translog.go util.go langs.go tools/importsumatra
+cd tools/importsumatra
 go build -o importsum *.go
 if [ "$?" -ne 0 ]; then echo "failed to build"; exit 1; fi 
 rm translog.go util.go langs.go # we used you so now we discard you
-cd ..
-./importsumatra/importsum
-rm importsumatra/importsum
+cd ../..
+./tools/importsumatra/importsum
+rm tools/importsumatra/importsum
