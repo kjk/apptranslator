a7537b2e5caca38d9d6b19d2509003251d60349c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-07-03 23:04:25 -0700

    remove TMPDIR/godep before building

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,6 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+rm -rf $TMPDIR/godep
 godep go build -o apptranslator_app *.go
 rm ./apptranslator_app
--- scripts/run.sh
+++ scripts/run.sh
@@ -4,6 +4,7 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+rm -rf $TMPDIR/godep
 godep go build -o apptranslator_app *.go
 ./apptranslator_app
 rm ./apptranslator_app
